1eef958159df4d2c03ccec9b3736350d0ea0e2e0
plamenti <plamenti_@abv.bg>
2015-03-26 19:43:03 +0200

    Added new methods from IProduct

--- LemonStorage/Classes/Product.cs
+++ LemonStorage/Classes/Product.cs
@@ -39,5 +39,55 @@
                 throw new System.NotImplementedException();
             }
         }
+
+
+        public string Model
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public System.ConsoleColor Color
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public void Save()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Load()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Buy()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Sell()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        int IProduct.Quantity()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
