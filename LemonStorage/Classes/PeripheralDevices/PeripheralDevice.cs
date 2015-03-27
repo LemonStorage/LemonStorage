c337b00368cd35372d522e58cc68db08ae1f6d7f
plamenti <plamenti_@abv.bg>
2015-03-27 19:21:26 +0200

    Added absent classes from PeripheralDevices

---
+++ LemonStorage/Classes/PeripheralDevices/PeripheralDevice.cs
@@ -0,0 +1,10 @@
+﻿namespace LemonStorage.Classes.PeripheralDevices
+{
+    using LemonStorage.Interfaces;
+    using LemonStorage.Enumerations;
+
+    public abstract class PeripheralDevice : Product, IProduct, IPeripheralDevices
+    {
+        
+    }
+}
