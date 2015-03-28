441a9eade17b5648a1b471e482d23a9034189062
plamenti <plamenti_@abv.bg>
2015-03-28 20:25:09 +0200

    Implemented class Printer

--- LemonStorage/Classes/PeripheralDevices/Printer.cs
+++ LemonStorage/Classes/PeripheralDevices/Printer.cs
@@ -1,8 +1,42 @@
 ﻿namespace LemonStorage.Classes.PeripheralDevices
 {
     using LemonStorage.Interfaces;
+    using System;
 
     public class Printer : PeripheralDevice, IProduct, IPeripheralDevices
     {
+        private string type;
+
+        public Printer(decimal price, string brand, string model, string color, string type)
+            :base(price, brand, model, color)
+        {
+            this.Type = type;
+        }
+
+        public string Type
+        {
+            get 
+            { 
+                return this.type; 
+            }
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("The type of the printer can not be empty");
+                }
+                this.type = value; 
+            }
+        }
+
+        public override void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
