8f0cb202ef1c663e702920ccb930b0d933223df4
plamenti <plamenti_@abv.bg>
2015-03-28 20:31:03 +0200

    Implemented class Scener

--- LemonStorage/Classes/PeripheralDevices/Scener.cs
+++ LemonStorage/Classes/PeripheralDevices/Scener.cs
@@ -1,8 +1,26 @@
 ﻿namespace LemonStorage.Classes.PeripheralDevices
 {
     using LemonStorage.Interfaces;
+    using System;
 
     public class Scener : PeripheralDevice, IProduct, IPeripheralDevices
     {
+        private int resolution;
+
+        public int Resolution
+        {
+            get 
+            { 
+                return this.resolution; 
+            }
+            set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("The scener resolution can not be less or equal to zero");
+                }
+                this.resolution = value; 
+            }
+        }
     }
 }
